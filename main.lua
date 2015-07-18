21b30c772e309dcef4b7088f24f3cb2cffc28110
changer <dingjinchengyx@163.com>
2015-07-18 11:16:39 +0800

    make demo

--- main.lua
+++ main.lua
@@ -2,9 +2,9 @@ print("LUA SCRIPTHAS BEEN OPEN");
 times = 0
 
 function lua_main()
-	if times > 60 then
+	if times > 1000 then
 		times = 0;
-		print("--------");	
+		print("lua script run");	
 	end
 	times = times+1;
 end
